0fa1d9d587d94238ede42cc63996e8ce6421e958
piv <USER@DOMAIN>
2009-06-24 08:27:13 +0000

    now templates vocabulary also works on adding form, in a dirty way so far, just do not know how to make it in a right way

--- collective/portlet/ngcollection/vocabularies.py
+++ collective/portlet/ngcollection/vocabularies.py
@@ -1,6 +1,8 @@
-from zope import component
-from zope.app.schema.vocabulary import IVocabularyFactory
 from zope.interface import implements
+from zope.component import queryUtility
+from zope.component.interfaces import IFactory
+from zope.app.container.interfaces import IAdding
+from zope.app.schema.vocabulary import IVocabularyFactory
 from zope.schema.vocabulary import SimpleTerm, SimpleVocabulary
 
 from collective.portlet.ngcollection.manager import getPortletTemplateManagers
@@ -8,12 +10,23 @@ from collective.portlet.ngcollection.interfaces import IPortletTemplateManager
 from collective.portlet.ngcollection import NGCollectionMessageFactory as _
 
 class PortletTemplates(object):
+
     implements(IVocabularyFactory)
 
     def __call__(self, context):
-        # TODO: how to make this vocabulary work while object is being
-        #       created, container but not portlet assignment is as
-        #       context at that moment
+        # we are on an adding form
+        if IAdding.providedBy(context):
+            # quick and dirty way to have a context on adding form
+            # is there some right solution for this?
+            factory_name = context.request.getURL().split('/')[-1]
+            if factory_name:
+                factory = queryUtility(IFactory, name=factory_name)
+                if factory is not None:
+                    try:
+                        context = factory()
+                    except Exception, e:
+                        # no luck, perhaps factory needs some arguments
+                        pass
         items = []
         for manager in getPortletTemplateManagers(context):
             items.extend([SimpleTerm(value, value, title)
